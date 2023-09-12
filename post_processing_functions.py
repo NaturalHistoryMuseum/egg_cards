8fac302109bce537f41cb77cb31b527be5092da0
Arianna <arianna.salili@gmail.com>
2023-09-12 02:35:20 +0100

    added backup function if key terms aren't found

--- post_processing_functions.py
+++ post_processing_functions.py
@@ -42,6 +42,24 @@ from fuzzywuzzy import fuzz
 ########
 # Key terms excluding taxonomic terms.
 key_terms = ["reg no", "locality", "collector", "date", "set mark", "no of eggs"]
+key_terms_extended = [
+    "reg no",
+    "reg. no",
+    "locality",
+    "collector",
+    "date",
+    "set mark",
+    "no of eggs",
+    "no. of eggs",
+]
+key_terms_p_ordered = [
+    "reg no",
+    "locality",
+    "collector",
+    "date",
+    "no of eggs",
+    "set mark",
+]
 taxon_terms = ["order", "family", "genus", "species", "canonicalName"]
 
 ###################
@@ -79,6 +97,133 @@ def get_text_index_for_keyterms(all_text_segments):
             results = "None"
         text_terms[u] = results
     return text_keys, text_terms
+
+
+def get_keyword_positions(text_segment):
+    # Input: text segment
+    # Output: list of first position (min_) and last position (max_) of key terms in text segment.
+    min_ = []
+    max_ = []
+    for k in key_terms_extended:
+        r_ = re.search(k, text_segment.lower())
+        if r_ is not None:
+            min_.append(r_.span()[0])
+            max_.append(r_.span()[1])
+    return min_, max_
+
+
+def define_breaks_in_text(min_, max_):
+    # Input: list of first position (min_) and last position (max_) of key terms in text segment.
+    # Output: position of breaks in the text.
+    min__ = min(min_)
+    breaks = [[0, min__]]
+
+    min_sorted = sorted(min_)
+    max_sorted = sorted(max_)
+
+    for i, mn in enumerate(min_sorted):
+        if i < len(min_sorted) - 1:
+            mx = min_sorted[i + 1]
+        else:
+            mx = -1
+        breaks.append([mn, mx])
+
+    return breaks
+
+
+def review_text_segments(all_text_segments, text_terms):
+    # Input: all text segments; dictionary of indexes containing key terms, per segment.
+    # Output: all text segments (possibly segmented further).
+    new_segmented_texts = []
+    for i, txt in enumerate(all_text_segments):
+        if type(text_terms[i]) == str:
+            new_segmented_texts.append(txt)
+        else:
+            if len(text_terms[i]) == 1:
+                new_segmented_texts.append(txt)
+            else:
+                min_, max_ = get_keyword_positions(txt)
+                breaks = define_breaks_in_text(min_, max_)
+                for b in breaks:
+                    new_segmented_texts.append(txt[b[0] : b[1]])
+    return new_segmented_texts
+
+
+############
+def get_missing_terms(text_keys):
+    # Input: dictionary of text indexes per key term.
+    # Output: missing key terms
+    missing_key_terms = []
+    found_keys = list(text_keys.keys())
+    for key_term in key_terms_p_ordered:
+        if key_term not in found_keys:
+            missing_key_terms.append(key_term)
+    return missing_key_terms
+
+
+def tuplewize(array, size):
+    # Input: text segment, number of words in key term
+    # Output: tuples of text segments (size based on the number of words in the key term).
+    if size < 2:
+        return np.array([array])
+
+    stack = np.stack([np.roll(array, -i) for i in range(size)])
+    return np.transpose(stack)[: -size + 1]
+
+
+def find_key_terms_backup(
+    all_text_segments, missing_key_terms, text_terms, fuzzy_threshold=80
+):
+    found_key_terms = {}
+    text_terms_copy = deepcopy(text_terms)
+    for keyword in missing_key_terms:
+        found = False
+        # Find number of words in the key term
+        n_word = len(re.findall("\w+", keyword))
+        for i, txt in enumerate(all_text_segments):
+            if type(text_terms_copy[i]) is str:
+                segment_words = re.findall("\w+", txt.lower())
+                if n_word > 1:
+                    tupled_words = tuplewize(segment_words, n_word)
+                else:
+                    tupled_words = deepcopy(segment_words)
+                for words in tupled_words:
+                    if n_word > 1:
+                        words_to_test = " ".join(map(str, words))
+                    else:
+                        words_to_test = deepcopy(words)
+                    ratio = fuzz.ratio(keyword, words_to_test)
+                    if ratio >= fuzzy_threshold:
+                        found = True
+                        break
+                if found:
+                    found_key_terms[keyword] = i
+                    text_terms_copy[i] = [keyword]
+                    break
+    return found_key_terms, text_terms_copy
+
+
+#########################################
+def process_text_into_segments(text):
+    # Input: original text output from OCR.
+    # Output: text processed into different segments, and indexes based on key terms found in segments.
+    all_text_segments = process_ocr_text(text)
+    # Get indexes related to key terms:
+    text_keys, text_terms = get_text_index_for_keyterms(all_text_segments)
+    # Review segments and make changes if needed:
+    new_segmented_texts = review_text_segments(all_text_segments, text_terms)
+    # Redo indexing, in case changes were made:
+    if all_text_segments != new_segmented_texts:
+        text_keys, text_terms = get_text_index_for_keyterms(new_segmented_texts)
+    # Check for missing key terms:
+    missing_key_terms = get_missing_terms(text_keys)
+    if len(missing_key_terms) > 0:
+        new_text_keys, text_terms = find_key_terms_backup(
+            new_segmented_texts, missing_key_terms, text_terms
+        )
+        if len(new_text_keys.keys()) > 0:
+            text_keys.update(new_text_keys)
+    return new_segmented_texts, text_keys, text_terms
 
 
 def get_term_related_text(all_text_segments, text_terms, term):
@@ -391,9 +536,7 @@ def get_all_egg_card_results(folder_path, filename):
     id_ = filename[:-4]
     text = np.load(path, allow_pickle=True)
     # Process results
-    all_text_segments = process_ocr_text(text)
-    # Get indexes related to key terms:
-    text_keys, text_terms = get_text_index_for_keyterms(all_text_segments)
+    all_text_segments, text_keys, text_terms = process_text_into_segments(text)
 
     # Extract data:
     all_results = {}
@@ -406,32 +549,47 @@ def get_all_egg_card_results(folder_path, filename):
     taxon = get_taxon_info(species_name, species_results)
     species_name_ = " ".join(map(str, re.findall("\S+", species_name)))
     # Add taxon info to dictionary
+    if len(species_name_) > 150:
+        species_name_ = "N/A"
     all_results["cardSpecies"] = species_name_
     for b in taxon.keys():
         all_results[b] = taxon[b]
 
     # 2. Registration number
     reg_no = find_reg_no_from_text(all_text_segments, text_keys)
+    if len(reg_no) > 60:
+        reg_no = reg_no[:60]
     all_results["registrationNumber"] = reg_no
 
     # 3. Locality.
     locality = get_locality(all_text_segments, text_terms)
+    if len(locality) > 150:
+        locality = "N/A"
     all_results["locality"] = locality
 
     # 4. Collector
     collector = get_collector(all_text_segments, text_terms)
+    if len(collector) > 200:
+        collector = "N/A"
     all_results["collector"] = collector
 
     # 5. Collection date
     date = get_date(all_text_segments, text_terms)
+    if len(date) > 60:
+        date = "N/A"
     all_results["date"] = date
 
     # 6. Set mark
     set_mark = get_setmark(all_text_segments, text_terms)
+    if len(set_mark) > 20:
+        set_mark = re.findall("\S+", set_mark)[0]
     all_results["setMark"] = set_mark
 
     # 7. Number of eggs
     no_eggs = get_no_of_eggs(all_text_segments, text_terms)
+    if len(no_eggs) > 20:
+        no_eggs = "N/A"
+        no_eggs = re.findall("\S+", no_eggs)[0]
     all_results["noOfEggs"] = no_eggs
 
     # 8. Other text
